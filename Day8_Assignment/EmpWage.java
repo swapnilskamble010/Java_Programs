e5c59b4abf0139189b4436de4d06d48fd8d04e2c
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-20 14:41:11 +0530

    Monthly Wage calculated

--- Day8_Assignment/EmpWage.java
+++ Day8_Assignment/EmpWage.java
@@ -19,28 +19,20 @@ public class EmpWage {
 			System.out.println("Employee is Absent");
 			this.empHour = 0;		
 		}
-//		if (empCheck == 1) {
-//			System.out.println("Employee is Present");
-//			this.empHour = 8;
-//		}
-//		else if (empCheck == 2) {
-//			System.out.println("Part time Employee is Present");
-//			this.empHour = 4; 	//Assuming part time hour 4
-//		}
-//		else {
-//			System.out.println("Employee is Absent");
-//			this.empHour = 0;
-//		}
 	}
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program using OOP's concepts");
 		
-		double empCheck = Math.floor(Math.random() * 10) % 3;
+		int totalEmpWage = 0;
 		EmpWage empWage = new EmpWage();
-		empWage.checkAttendance(empCheck);
-		int dailyEmpWage = empWage.WAGE_PER_HOUR * empWage.empHour;
-		System.out.println("Daily Wage of Employee is : " + dailyEmpWage);
-		
+		for (int i = 1; i < 21; i++) {
+			double empCheck = Math.floor(Math.random() * 10) % 3;
+			empWage.checkAttendance(empCheck);
+			int dailyEmpWage = empWage.WAGE_PER_HOUR * empWage.empHour;
+			System.out.println("Daily Wage of Employee is : " + dailyEmpWage);
+			totalEmpWage += dailyEmpWage;
+			System.out.println("Monthly Wage of Employee is : " + totalEmpWage);
+		}	
 	}
 }
