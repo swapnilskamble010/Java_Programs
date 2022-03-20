f57a164c64adc08cedd5e49e89c0dbf3fdeac632
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-20 14:02:46 +0530

    Daily Employee Wage calculated

--- Day8_Assignment/EmpWage.java
+++ Day8_Assignment/EmpWage.java
@@ -1,16 +1,27 @@
 package day_8;
 
 public class EmpWage {
+	final byte WAGE_PER_HOUR = 20;
+	byte empHour = 0;
+	void checkAttendance(double empCheck) {
+		if (empCheck == 1) {
+			System.out.println("Employee is Present");
+			this.empHour = 8;
+		}	
+		else {
+			System.out.println("Employee is Absent");
+			this.empHour = 0;
+		}
+	}
+	
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program using OOP's concepts");
 		
 		double empCheck = Math.floor(Math.random() * 10) % 2;
+		EmpWage empWage = new EmpWage();
+		empWage.checkAttendance(empCheck);
+		int dailyEmpWage = empWage.WAGE_PER_HOUR * empWage.empHour;
+		System.out.println("Daily Wage of Employee is : " + dailyEmpWage);
 		
-		if (empCheck == 1) {
-			System.out.println("Employee is Present");	
-		}	
-		else {
-			System.out.println("Employee is Absent");
-		}
 	}
 }
