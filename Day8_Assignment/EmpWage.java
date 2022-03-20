d9f381614d1953dcecfeda24ee6141e462c3307b
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-20 13:24:03 +0530

    Checking Employee is present or absent

--- Day8_Assignment/EmpWage.java
+++ Day8_Assignment/EmpWage.java
@@ -3,6 +3,14 @@ package day_8;
 public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program using OOP's concepts");
+		
+		double empCheck = Math.floor(Math.random() * 10) % 2;
+		
+		if (empCheck == 1) {
+			System.out.println("Employee is Present");	
+		}	
+		else {
+			System.out.println("Employee is Absent");
+		}
 	}
-
 }
