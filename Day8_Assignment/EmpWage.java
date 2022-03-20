e71228383bb19f65e1ff6f00312c3bcd285ff170
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-20 16:21:24 +0530

    Wage calculation till a condition is reached

--- Day8_Assignment/EmpWage.java
+++ Day8_Assignment/EmpWage.java
@@ -3,16 +3,19 @@ package day_8;
 public class EmpWage {
 	final byte WAGE_PER_HOUR = 20;
 	byte empHour = 0;
+	byte workingDaysCount = 0;
 	void checkAttendance(double empCheck) {
 		switch((int)empCheck) {
 		case 1:
 			System.out.println("Employee is Present");
 			this.empHour = 8;
+			workingDaysCount++;
 			break;
 			
 		case 2:
 			System.out.println("Part time Employee is Present");
 			this.empHour = 4;
+			workingDaysCount++;
 			break;
 			
 		default:
@@ -25,13 +28,20 @@ public class EmpWage {
 		System.out.println("Welcome to Employee Wage Computation Program using OOP's concepts");
 		
 		int totalEmpWage = 0;
+		int totalEmpHour = 0;
 		EmpWage empWage = new EmpWage();
-		for (int i = 1; i < 21; i++) {
+		while (empWage.workingDaysCount < 21 || totalEmpHour < 101){
+			if (empWage.workingDaysCount > 20 || totalEmpHour > 100){
+				break;
+			}
 			double empCheck = Math.floor(Math.random() * 10) % 3;
 			empWage.checkAttendance(empCheck);
 			int dailyEmpWage = empWage.WAGE_PER_HOUR * empWage.empHour;
-			System.out.println("Daily Wage of Employee is : " + dailyEmpWage);
-			totalEmpWage += dailyEmpWage;
+			//System.out.println("Daily Wage of Employee is : " + dailyEmpWage);
+			totalEmpHour += empWage.empHour;
+			System.out.println("Total Employe Hour is: " + totalEmpHour);
+			System.out.println("Total Working Days of Employee is : " + empWage.workingDaysCount);
+			totalEmpWage += dailyEmpWage;				
 			System.out.println("Monthly Wage of Employee is : " + totalEmpWage);
 		}	
 	}
