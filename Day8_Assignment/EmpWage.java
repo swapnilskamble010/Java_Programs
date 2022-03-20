714dcd2331c7378930709aa1cd41deac16f96df9
Swapnil_Kamble <swapnilskamble010@gmail.com>
2022-03-20 14:11:33 +0530

    Part time Employee added and Employee Wage calculated

--- Day8_Assignment/EmpWage.java
+++ Day8_Assignment/EmpWage.java
@@ -7,7 +7,11 @@ public class EmpWage {
 		if (empCheck == 1) {
 			System.out.println("Employee is Present");
 			this.empHour = 8;
-		}	
+		}
+		else if (empCheck == 2) {
+			System.out.println("Part time Employee is Present");
+			this.empHour = 4; 	//Assuming part time hour 4
+		}
 		else {
 			System.out.println("Employee is Absent");
 			this.empHour = 0;
@@ -17,7 +21,7 @@ public class EmpWage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to Employee Wage Computation Program using OOP's concepts");
 		
-		double empCheck = Math.floor(Math.random() * 10) % 2;
+		double empCheck = Math.floor(Math.random() * 10) % 3;
 		EmpWage empWage = new EmpWage();
 		empWage.checkAttendance(empCheck);
 		int dailyEmpWage = empWage.WAGE_PER_HOUR * empWage.empHour;
